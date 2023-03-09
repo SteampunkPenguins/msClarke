b0e803c8371786099764131f43885496cdc92fb1
tquan99 <126923588+tquan99@users.noreply.github.com>
2023-03-09 00:05:25 -0500

    Update Robot.java
    This is an example for retrieving values from the NavX or a Spark Max encoder and subsequently sending them to a dashboard. The values should be visible on both SmartDashboard and Shuffleboard (depending on which one you select from the Driver Station). The code for a "sendable chooser" was pre-loaded into the template, but could be useful for selecting different Autonomous modes.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,134 +1,178 @@
-/* This is the master code for the robot. At this time it is not modulized
- * Add comments for each thing that you add. Try and explain what it does and where it gets
- * the parameters which it uses.
- */
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Compressor;
- 
- 
- public class Robot extends TimedRobot {
-   // Drive Train Motors
-   // Each motor in the drive train is declared
-   private final CANSparkMax frontLeft = new CANSparkMax(0, MotorType.kBrushless); //CAN ID is set on the sparkmax 
-   private final CANSparkMax backLeft = new CANSparkMax(1, MotorType.kBrushless); // Motortype must also be set for the neo it is brushless.
-   private final CANSparkMax frontRight = new CANSparkMax(2, MotorType.kBrushless);
-   private final CANSparkMax backRight = new CANSparkMax(3, MotorType.kBrushless);
-   // Two motor control groups are declared to control the left and right side of the robot as one group.
-   private final MotorControllerGroup leftDrive = new MotorControllerGroup(frontLeft, backLeft);
-   private final MotorControllerGroup rightDrive = new MotorControllerGroup(frontRight, backRight);
-   // the Differential Drive is declared and named msClarke
-   private final DifferentialDrive msClarke = new DifferentialDrive(leftDrive, rightDrive); //the two motor control groups are added to the differential drive as parameters
-   // Arm/Claw Motors
-   private final CANSparkMax leftIntake = new CANSparkMax(4, MotorType.kBrushless);
-   private final CANSparkMax rightIntake = new CANSparkMax(5, MotorType.kBrushless);
-   //private final DifferentialDrive clawIntake = new DifferentialDrive(leftIntake, rightIntake);
-   private final CANSparkMax teleScope = new CANSparkMax(6, MotorType.kBrushless);
-   private final CANSparkMax tiltArm = new CANSparkMax(7, MotorType.kBrushless);
-   
-   //Solenoids
-   private final DoubleSolenoid m_doubleSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
-   private final DoubleSolenoid secondSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 3, 4);
-   //private static final int kSolenoidButton = 1;
-   //private static final int kDoubleSolenoidForward = 2; //the button which operates the solenoid
-   //private static final int kDoubleSolenoidReverse = 3;
-   // Compressor Varibales [RO] still working on figuring this out.
-   Compressor pcmCompressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
-   //Compressor phCompressor = new Compressor(1, PneumaticsModuleType.REVPH);
-   //pcmCompressor.enableDigital();
-   //pcmCompressor.disable();
-   //boolean enabled = pcmCompressor.IsEnabled();
-   //boolean pressureSwitch = pcmCompressor.getPressureSwitchValue();
-   //double current = pcmCompressor.getCompressorCurrent();
-   
-   // Other variables
-   private final XboxController driver = new XboxController(0); //Xbox controller is usually detected in port 0
-   private final XboxController armController = new XboxController(1); 
-   private final Timer m_timer = new Timer(); // a timer is needed for autonomous mode
- 
-   //This function is run when the robot is first started up and should be used for any initialization code.
-   @Override
-   public void robotInit() {
-     rightDrive.setInverted(true); //On side of the robot's drivetrain must be inverted so that the motors can turn in same relative direction.
-     //rightIntake.setInverted(true);
-     leftIntake.follow(rightIntake, true);
-   }
- 
-   //This function is run once each time the robot enters autonomous mode.
-   @Override
-   public void autonomousInit() {
-     m_timer.reset();
-     m_timer.start();
-   }
- 
-   //This function is called periodically (20ms) during autonomous.
-   @Override
-   public void autonomousPeriodic() {
-     // Drive for 2 seconds
-     if (m_timer.get() < 15.0) {
-       // Drive forwards half speed, make sure to turn input squaring off
-       msClarke.arcadeDrive(0.5, 0.0, isAutonomous());
-     } else {
-       msClarke.stopMotor(); // stop robot
-     }
-   }
- 
-   //This function is called once each time the robot enters teleoperated mode.
-   @Override
-   public void teleopInit() {
-    pcmCompressor.enableDigital();
-   }
- 
-   //This function is called periodically during teleoperated mode.
-   @Override
-   public void teleopPeriodic() {
-     //Drive Train controls
-     msClarke.arcadeDrive(-driver.getLeftY(), -driver.getRightX());
-     // Arm Controls
-     teleScope.set(armController.getLeftY());
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-     tiltArm.set(armController.getRightY());
-     // Claw Controls
-    // clawIntake.arcadeDrive(m_controller.getLeftTriggerAxis(), 0.0);
-    if (armController.getBButtonPressed()) {
-        rightIntake.set(0.5);
+//Encoders are part of the REV Robotics vendor library
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+//NavX library and serial communication protocol. A Timer is used for debouncing.
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SerialPort;
+
+import edu.wpi.first.wpilibj.Timer;
+
+//RoboRIO camera library
+import edu.wpi.first.cameraserver.CameraServer;
+ 
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+//Each individual encoder is associated with a specific CANSparkMax
+  private final CANSparkMax frontLeft = new CANSparkMax(0, MotorType.kBrushless);
+  private final RelativeEncoder m_encoder = frontLeft.getEncoder();
+
+  //NavX
+  private final AHRS navX = new AHRS(SerialPort.Port.kMXP);
+  
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+   
+    //Allows the NavX to be readable by the dashboard
+    navX.enableLogging(true);
+
+    //Enables camera server on dashboard
+    CameraServer.startAutomaticCapture(0);
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {}
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
     }
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+
+
+
+    while (isTeleopEnabled()) {
+            
+      //Encoder dashboard readouts
+      SmartDashboard.putNumber("frontLeft Position", m_encoder.getPosition());
+      SmartDashboard.putNumber("frontLeft Velocity", m_encoder.getVelocity());
+
+
+      //NavX dashboard readouts
+      Timer.delay(0.020);		/* wait for one motor update time period (50Hz)     */
+      
+      boolean zero_yaw_pressed = false; //stick.getTrigger();
+      if ( zero_yaw_pressed ) {
+          navX.zeroYaw();
+      }
+
+      /* Display 6-axis Processed Angle Data                                      */
+      SmartDashboard.putBoolean(  "IMU_Connected",        navX.isConnected());
+      SmartDashboard.putNumber(   "IMU_Yaw",              navX.getYaw());
+      SmartDashboard.putNumber(   "IMU_Pitch",            navX.getPitch());
+      SmartDashboard.putNumber(   "IMU_Roll",             navX.getRoll());
  
-     // Extend Pneumatic Cylinder
-     //if (armController.getYButtonPressed()) { // use the Y button to toggle the claw open or closed.
-     //  m_doubleSolenoid.toggle();
-     //}
-     
-     if (armController.getRightBumperPressed()) { // use the A button to extend the cylinder
-       m_doubleSolenoid.set(DoubleSolenoid.Value.kForward);
-       secondSolenoid.set(DoubleSolenoid.Value.kForward);
-     } else if (armController.getLeftBumperPressed()) { // use the B button to retract the cylinder
-       m_doubleSolenoid.set(DoubleSolenoid.Value.kReverse);
-       secondSolenoid.set(DoubleSolenoid.Value.kReverse);
-   }
- }
- 
-   //This function is called once each time the robot enters test mode.
-   @Override
-   public void testInit() {}
- 
-   //This function is called periodically during test mode.
-   @Override
-   public void testPeriodic() {}
- }
- 
- 
- 
- /* CHANGE LOG enter the date and changes made each time you edit this code.
-  * 
-  */
+
+      /* These functions are compatible w/the WPI Gyro Class, providing a simple  */
+      /* path for upgrading from the Kit-of-Parts gyro to the navx MXP            */
+      
+      SmartDashboard.putNumber(   "IMU_TotalYaw",         navX.getAngle());
+      SmartDashboard.putNumber(   "IMU_YawRateDPS",       navX.getRate());
+      
+      /* Quaternion Data                                                          */
+      /* Quaternions are fascinating, and are the most compact representation of  */
+      /* orientation data.  All of the Yaw, Pitch and Roll Values can be derived  */
+      /* from the Quaternions.  If interested in motion processing, knowledge of  */
+      /* Quaternions is highly recommended.                                       */
+      SmartDashboard.putNumber(   "QuaternionW",          navX.getQuaternionW());
+      SmartDashboard.putNumber(   "QuaternionX",          navX.getQuaternionX());
+      SmartDashboard.putNumber(   "QuaternionY",          navX.getQuaternionY());
+      SmartDashboard.putNumber(   "QuaternionZ",          navX.getQuaternionZ());
+  }
+
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
